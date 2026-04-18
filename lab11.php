dd0ed198a38d088efe1ad869f228c906b3a5479a
Veena Shinde <veenashinde612@gmail.com>
2026-04-18 19:17:28 +0530

    Add files via upload

---
+++ lab11.php
@@ -0,0 +1,10 @@
+<?php
+$str = "PHP Programming";
+
+echo "Original String: $str <br>";
+echo "Length: " . strlen($str) . "<br>";
+echo "Reverse: " . strrev($str) . "<br>";
+echo "Substring: " . substr($str, 0, 3);
+?>
+
+
